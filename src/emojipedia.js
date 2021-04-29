193047e43e0c59169f560dc60476564960415b80
Akhilesh Yadav <akhileshyadavkokta@gmail.com>
2021-04-29 12:20:22 +0530

    Commited

---
+++ src/emojipedia.js
@@ -0,0 +1,68 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "💓",
+    name: "Red Heart",
+    meaning:
+      "The red heart is the classical symbol of love. Expression of passion and romance. But also in the non-romantic context for friendship and strong bonds."
+  },
+  {
+    id: 5,
+    emoji: "💔",
+    name: " Broken Heart",
+    meaning:
+      "I am desperately unhappy! The broken red heart expresses sadness and suffering. Someone is love sick at the moment."
+  },
+  {
+    id: 6,
+    emoji: "💘 ",
+    name: "Heart With Arrow",
+    meaning:
+      "Cupid's arrow hit you - you're hopelessly in love! Expresses romantic feelings in messages. Often used around Valentine's Day."
+  },
+  {
+    id: 7,
+    emoji: "😂 ",
+    name: "Face With Tears of Joy",
+    meaning:
+      "I am laughing to hard that I can hardly keep myself on my chair! Something is so incredibly funny that you cry laughing. Gets the giggles and can hardly contain himself. This emoji is the most popular and was named Word of the Year 2015"
+  },
+  {
+    id: 8,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 9,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  }
+];
+
+export default emojipedia;
